339d0fca6ec19f7a63653d7bfd48e95b366858d5
Thiago Rocha <thiago_srocha01@hotmail.com>
2024-08-12 02:48:23 -0300

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,35 @@
 import React from "react"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 const Home = () => {
   return (
     <div>
-      <h1 className="m-5">
-        <Button>Button</Button>
-      </h1>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-semibold">Olá, Thiago</h2>
+        <p className="text-sm">Domingo, 11 de Agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button size="icon">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com a FSW Barber "
+            src={"/banner-01.png"}
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
