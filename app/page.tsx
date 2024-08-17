48c58ecfe8ba3eeacfa1951818e69e92675cbafa
Thiago Rocha <thiago_srocha01@hotmail.com>
2024-08-17 14:34:59 -0300

    feat: add BarbershopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,12 @@ import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item"
 
-const Home = () => {
+const Home = async () => {
+  const barbershops = await db.barbershop.findMany()
+
   return (
     <div>
       {/* header */}
@@ -36,7 +40,8 @@ const Home = () => {
         </div>
 
         {/* Agendamento */}
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">Agendamento</h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* esquerda */}
             <div className="flex flex-col gap-2 py-5 pl-5">
@@ -57,6 +62,15 @@ const Home = () => {
             </div>
           </CardContent>
         </Card>
+
+        {/* Recomendados */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">Recomendados</h2>
+        {/* <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden"> */}
+        <div className="flex gap-4 overflow-auto">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   )
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
+      },
+      {
+        hostname: "i.pinimg.com",
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
